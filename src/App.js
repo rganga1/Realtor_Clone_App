92fe7f4061f2ae58ec6162297888301408aeea93
rakesh-ganga <rakesh.g@innomick.com>
2023-04-19 18:43:38 +0530

    routes and pages
    Installed 2 router modules, Added 6 routes and 6 corresponding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SingUp from "./pages/SingUp"
+import SingIn from "./pages/SingIn"
+import ForgotPassword from "./pages/ForgotPassword"
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl text-blue-600'> hello world</h1>
-    </div>
+    // <div className="App">
+    //   <h1 className='text-2xl text-blue-600'> hello world</h1>
+    // </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SingIn />} />
+          <Route path="/sign-up" element={<SingUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

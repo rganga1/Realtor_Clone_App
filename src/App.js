96cc15e310dbe62c2cb3b42e329a10a30487886b
rakesh-ganga <rakesh.g@innomick.com>
2023-04-24 17:44:48 +0530

    added functionality for form sell/rent

--- src/App.js
+++ src/App.js
@@ -18,13 +18,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<PrivateRoute />}>
-            <Route index element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer

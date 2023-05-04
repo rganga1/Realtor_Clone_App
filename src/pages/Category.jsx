58ada128a3bdbb2fbf60559ac331cd05b56e9278
rakesh-ganga <rakesh.g@innomick.com>
2023-05-04 18:27:35 +0530

    created slider component, home-page, offers-page, rent and sales page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,19 +12,20 @@ import {
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
-import { async } from "@firebase/util";
+import { useParams } from "react-router-dom";
 
-export default function Offers() {
+export default function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
+  const params = useParams();
   useEffect(() => {
     async function fetchListings() {
       try {
         const listingRef = collection(db, "listings");
         const q = query(
           listingRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamp", "desc"),
           limit(8)
         );
@@ -46,14 +47,14 @@ export default function Offers() {
     }
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
 
   async function onFetchMoreListings() {
     try {
       const listingRef = collection(db, "listings");
       const q = query(
         listingRef,
-        where("offer", "==", true),
+        where("type", "==", params.categoryName),
         orderBy("timestamp", "desc"),
         startAfter(lastFetchedListing),
         limit(4)
@@ -68,7 +69,7 @@ export default function Offers() {
           data: doc.data(),
         });
       });
-      setListings((prevState)=>[...prevState, ...listings]);
+      setListings((prevState) => [...prevState, ...listings]);
       setLoading(false);
     } catch (error) {
       toast.error("Could not fetch listing");
@@ -77,7 +78,9 @@ export default function Offers() {
 
   return (
     <div className="max-w-6xl px-3 mx-auto">
-      <h1 className="mt-6 mb-6 text-3xl font-bold text-center">Offers</h1>
+      <h1 className="mt-6 mb-6 text-3xl font-bold text-center">
+        {params.categoryName === "rent" ? "Places for rent" : "Places for sale"}
+      </h1>
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
